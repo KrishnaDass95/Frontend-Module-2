b66179e5f0900341cb0a60faa84547cec0c21d96
KrishnaDass95 <krishdass95@gmail.com>
2023-02-09 12:10:39 +0530

    functions

---
+++ Js-Day-1/functions.js
@@ -0,0 +1,13 @@
+// requirement is to print numbers from 2- 10, 40-50, 22- 25;
+// instead of creating for loop 3 times, we can write one function to solve the entire thing
+// they are used to execute a logic or run a code block again and again
+
+// Syntax -> use function keyword, function name,
+function forLoops(start, end, step){
+    for(var i = start; i <= end; i += step)
+        {
+        console.log(i);
+        }
+}
+forLoops(40, 50, 2);
+// functions help with reusability
