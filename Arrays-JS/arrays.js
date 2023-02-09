7fa77eecb191874d6185260100a2472c29bf2b35
KrishnaDass95 <krishdass95@gmail.com>
2023-02-09 20:30:25 +0530

    Arrays and its functions

--- Arrays-JS/arrays.js
+++ Arrays-JS/arrays.js
@@ -16,3 +16,30 @@ console.log(marks); // [ 100, <3 empty items>, 500 ] ouput
 // you can also have multiple data types inside the array
 var assorted = ["krishna", 100, undefined];
 console.log(assorted);
+
+// delete the starting element in your array using the shift system, it deletes the starting value and shifts one to the right
+assorted.shift();
+console.log(assorted);
+
+// how to add an item in the beginning in JS array, it creates a container one to the left 
+assorted.unshift("krishna!");
+console.log(assorted);
+
+console.log("---------");
+// how to iterate through the loop? you can use the arrayName.length to get the size of the array
+// first approach 
+for(var i = 0; i < assorted.length; i++)
+{
+    console.log(assorted[i]);
+}
+
+// for each loop, goes to every index and then it performs its task.
+function logValues(value){
+    console.log(value);
+}
+assorted.forEach(logValues)
+
+// another way to iterate through
+console.log("---------");
+var books = ["freedom", "cycle", "run forever"];
+books.forEach(function (books, index) { console.log(books, index)})
