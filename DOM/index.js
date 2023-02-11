b61767cf52c9829bfff20d4bf3f69eb2d6a26ce4
KrishnaDass95 <krishdass95@gmail.com>
2023-02-11 12:47:27 +0530

    Practice adding a child element

--- DOM/index.js
+++ DOM/index.js
@@ -13,12 +13,25 @@ console.log(HTML);
 var containerDiv = document.querySelector('.container');
 console.log("containerDiv", containerDiv);
 
-// now lets create a button which is a DOM object
+// now lets create a HTML element button which is a DOM object
 var button = document.createElement('button'); // DOM object
 button.textContent = "Click me";
+button.style.color = "red";
 
 // now let's add the  button the container div
 
 containerDiv.appendChild(button);
 
 
+// lets practice the above one more time
+var containerPractice = document.querySelector('.container-practice');
+var h3 = document.createElement('h3');
+h3.textContent = "I am a H3 DOM Object";
+containerPractice.appendChild(h3);
+
+function addH3Text(parent, text){
+    var element = document.createElement('h3');
+    element.textContent = text;
+    parent.appendChild(element);
+}
+addH3Text(containerPractice, "Hi Krishna");
