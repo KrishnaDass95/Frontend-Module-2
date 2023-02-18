1a750e93b45892b401a1086d3bbf5afb9fd6e5c7
KrishnaDass95 <krishdass95@gmail.com>
2023-02-18 12:09:48 +0530

    Get requirements

---
+++ F2-Contest2/index.js
@@ -0,0 +1,9 @@
+// TODO
+
+//1. create an array with an object
+//1.5 need to post the content of the form into the table
+//2. default behaviour should be that issued book status is not returned
+//3. need a JS library to output current date and time
+//4. The status of the book should be editable
+//5. A JS function that changes status of the books to be returned or not
+//6. Change color on the status basis
