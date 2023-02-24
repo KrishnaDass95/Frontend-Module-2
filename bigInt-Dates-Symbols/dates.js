06c739c3c6444d34985b538a2f9cda091abd4cbf
KrishnaDass95 <krishdass95@gmail.com>
2023-02-24 13:51:09 +0530

    EPOC time and date object in JS

---
+++ bigInt-Dates-Symbols/dates.js
@@ -0,0 +1,10 @@
+// use the data object in JS to get the current date and time. 
+// use the date objects functions to get all the details
+
+var dateAndTime = new Date();
+console.log(dateAndTime);
+console.log(dateAndTime.getTime()); // gives you EPOC time
+// EPOC time is the number of seconds 00:00:00 that has passed from UTC from 1970, used for logging and systems to understand when in time this happened
+
+
+//TODO ->  explore and play around with some functions of the Date object
