5dc14a919de02388056f19d468358167e9f1506b
KrishnaDass95 <krishdass95@gmail.com>
2023-02-25 17:06:39 +0530

    Fix id numbering for each row

--- F2-Contest3/index.js
+++ F2-Contest3/index.js
@@ -15,18 +15,35 @@ function createSaveButton(){
     const saveBtn = document.createElement('button');
     saveBtn.innerText = 'Save';
     saveBtn.classList.add('btn');
+    return saveBtn;
 }
 
 function addDataToRow(row){
-    const lastEntry = tableData.length - 1;
+    let lastEntry = tableData.length - 1;
     const keys = Object.keys(tableData[lastEntry]);
     keys.forEach(function(key){
+        if(key == 'id'){
+        const td = document.createElement('td');
+        row.appendChild(td);
+        const lastIdValue = tableData[lastEntry].id;
+        const idValue = lastIdValue+1;
+        lastEntry = tableData.length - 1;
+        tableData[lastEntry].id = idValue;
+        td.textContent = idValue;
+        }
+        else if(key == 'save'){
+            createSaveButton();
+        }
+        else{
         const td = document.createElement('td');
         row.appendChild(td);
         const inputField = document.createElement('input');
         td.appendChild(inputField);
         inputField.setAttribute('type', 'text');
         inputField.classList.add('input-field');
+        }
+
+        
     })
     
 
