d4348210a130b143b9bc9aa8a965d2537686bad5
KrishnaDass95 <krishdass95@gmail.com>
2023-03-01 11:56:47 +0530

    Add divider lines into the game

--- CarGameProject/app.js
+++ CarGameProject/app.js
@@ -12,6 +12,17 @@ function startGame(){
 
     // we need this car to be inside the game container
     gameContainer.appendChild(car);
+
+    // next we need to add the road-divider lines
+    // at any given point, there are 4 lines present
+    let top = 0;
+    for(let i = 0; i < 4; i++){
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = top + 'px';
+        gameContainer.appendChild(line);
+        top += 150;
+    }
 }
 
 
