02896c34e371d20f8203d0253d42a495c65f8b7b
KrishnaDass95 <krishdass95@gmail.com>
2023-03-01 11:38:34 +0530

    Let's add a car, just a div block for now

--- CarGameProject/app.js
+++ CarGameProject/app.js
@@ -3,7 +3,15 @@ const startContainer = document.querySelector('.start');
 const gameContainer = document.querySelector('.game');
 
 function startGame(){
+    // hide the start container
     startContainer.classList.add('hide');
+
+    // let's add a car now
+    const car = document.createElement('div');
+    car.setAttribute('class', 'car');
+
+    // we need this car to be inside the game container
+    gameContainer.appendChild(car);
 }
 
 
