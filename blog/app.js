78c94634744dedbf474652482bc6486ee960ab31
KrishnaDass95 <krishdass95@gmail.com>
2023-03-04 12:32:53 +0530

    Add all the submitted data to object and create a date function

--- blog/app.js
+++ blog/app.js
@@ -7,27 +7,45 @@ let blogs = [];
 
 
 
+function addBlogToPage(obj){
+    
+}
 
+function getDateAndTime(){
+    const d = new Date();
+    const date = d.getDate();
+    const month = d.getMonth();
+    const year = d.getFullYear();
+    const time = d.toLocaleTimeString();
+    return `${date}/${month}/${year} at ${time}`;
+}
 
+// close the modal when open
 function closeModal(modal){
     modal.style.display = 'none';
 }
 
-
+// Creates a new Blog
 function createNewBlog(){
     newPostModalElement.style.display = 'block'
     const cancelButton = document.querySelector('#cancel');
     const crossButton = document.querySelector('.close');
-
-    let blogsObj = {
-
-    }
     const publishButtonElement = document.querySelector('#publish');
 
     publishButtonElement.addEventListener('click', ()=> {
-        const blogTitle = document.querySelector('.blog-title').value;
-        const blogDesc = document.querySelector('.blog-desc').value;
-        alert(blogTitle + blogDesc);
+        let blogTitle = document.querySelector('.blog-title').value;
+        let blogDesc = document.querySelector('.blog-desc').value;
+        let blogsObj = {
+            id: Math.random().toString(36).substring(2,8),
+            title: blogTitle,
+            description: blogDesc,
+            date: `created At: ${getDateAndTime()}`,
+        }
+        // for debugging
+        // console.log(blogsObj); 
+        blogs.push(blogsObj);
+        addBlogToPage(blogsObj);
+        // TODO clear the fields once submitted
     });
 
     cancelButton.addEventListener('click', ()=>{
