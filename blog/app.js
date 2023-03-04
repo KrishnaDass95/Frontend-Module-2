53f53a7185917ecf768b37dc31fd43acb8d5dbe7
KrishnaDass95 <krishdass95@gmail.com>
2023-03-04 13:04:27 +0530

    Add Blog content from page to Modal

--- blog/app.js
+++ blog/app.js
@@ -1,15 +1,40 @@
 // get all the elements
 const createNewPostElement = document.querySelector('#new-post');
 const newPostModalElement = document.querySelector('.modal-new-post');
+const blogContainerElement = document.querySelector('.blog-container');
 
 // empty array to store all the blogs
 let blogs = [];
 
 
 
-function addBlogToPage(obj){
-    
-}
+function addBlogToPage(obj) {
+    const blogElement = document.createElement("div");
+    blogElement.classList.add("blog");
+  
+    const headingElement = document.createElement("h3");
+    headingElement.textContent = obj.title;
+    blogElement.appendChild(headingElement);
+  
+    const paragraphElement = document.createElement("p");
+    paragraphElement.textContent = obj.description;
+    blogElement.appendChild(paragraphElement);
+  
+    const editButton = document.createElement("button");
+    editButton.textContent = "Edit Post";
+    blogElement.appendChild(editButton);
+  
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete Post";
+    blogElement.appendChild(deleteButton);
+  
+    const dateElement = document.createElement("span");
+    dateElement.textContent = obj.date;
+    blogElement.appendChild(dateElement);
+  
+    blogContainerElement.appendChild(blogElement);
+  }
+  
 
 function getDateAndTime(){
     const d = new Date();
